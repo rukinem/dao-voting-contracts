925c04ced14a8c9502565dcfbffb8549805babfb
rukinem <rukinem@users.noreply.github.com>
2026-05-04 10:16:09 +0800

    feat: complete dao contract + Next.js frontend

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
-contract DaoVotingContracts is Ownable {
-    // TODO: Implement Gas-optimized voting contracts with snapshot, quadratic and conviction voting
-    constructor() Ownable(msg.sender) {}
+contract DaoVotingContracts is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorTimelockControl {
+    uint256 public constant QUORUM_PERCENTAGE = 4;
+
+    constructor(string memory name_, IVotes token, TimelockController timelock, uint48 initialVotingDelay, uint32 initialVotingPeriod, uint256 initialProposalThreshold)
+        Governor(name_) GovernorSettings(initialVotingDelay, initialVotingPeriod, initialProposalThreshold) GovernorVotes(token) GovernorTimelockControl(timelock) {}
+
+    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description) public override returns (uint256) {
+        return super.propose(targets, values, calldatas, description);
+    }
+
+    function votingDelay() public view override(Governor, GovernorSettings) returns (uint256) { return super.votingDelay(); }
+    function votingPeriod() public view override(Governor, GovernorSettings) returns (uint256) { return super.votingPeriod(); }
+    function quorum(uint256 blockNumber) public view override(Governor, GovernorCountingSimple) returns (uint256) { return totalSupply(blockNumber) * QUORUM_PERCENTAGE / 100; }
+    function state(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (ProposalState) { return super.state(proposalId); }
+    function proposalNeedsQueuing(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (bool) { return super.proposalNeedsQueuing(proposalId); }
+    function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) { return super.proposalThreshold(); }
 }
